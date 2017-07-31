3d6fcb3c274fec1cf244dd0c62c432059b882bd2
Marina Boeva <camado6pota@yandex.ru>
2017-07-31 19:15:54 +0300

    basic marckup

--- backery/gulpfile.js
+++ backery/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     sass = require('gulp-sass');
 
 gulp.task('sass',function () {
-  return gulp.src('app/sass/*.sass')
+  return gulp.src('app/sass/main.sass')
   .pipe(sass())
   .pipe(gulp.dest('app/css'))
 });
